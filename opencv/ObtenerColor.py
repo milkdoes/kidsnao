5d30257a81e526396e6bdfb0122f903d8cf5319d
SalcedoJose <josemasal@gmail.com>
2018-03-19 08:29:56 -0700

    Script de obtener color actualizado para recibir imagen como argumento y retornar valores rgb al ser llamado.

--- opencv/ObtenerColor.py
+++ opencv/ObtenerColor.py
@@ -1,13 +1,26 @@
+#!/usr/bin/python
+# Uso: Dar ruta de imagen al llamar script:
+# python /ruta/a/imagen.extension
+import sys
 import cv2
+import json
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt  # noqa
 from sklearn.cluster import KMeans
 
 
+# ------IMPORTAR LIBRERAS matplot y kmeans ---------------
 
-## ------IMPORTAR LIBRERAS matplot y kmeans ---------------
+# Definir ruta de imagen dada como argumento.
+rutaImagen = ""
+if (len(sys.argv) >= 2):
+    rutaImagen = sys.argv[1]
 
+# GLOBALES.
 posicionMayor = 0
+valoresRgbMayoritarios = [0, 0, 0]
+
+
 def find_histogram(clt):
     """
     create a histogram with k clusters
@@ -19,21 +32,22 @@ def find_histogram(clt):
 
     hist = hist.astype("float")
     hist /= hist.sum()
-    
+
     mayor = ("%.4f" % max(hist))
-    
-    i=0
-     
+
+    i = 0
+
     for n in hist:
-        
         numero = ("%.4f" % n)
         if (numero == mayor):
             global posicionMayor
-            posicionMayor = i;
-        
+            posicionMayor = i
+
         i = i + 1
 
     return hist
+
+
 def plot_colors2(hist, centroids):
     bar = np.zeros((50, 300, 3), dtype="uint8")
     startX = 0
@@ -44,24 +58,47 @@ def plot_colors2(hist, centroids):
         cv2.rectangle(bar, (int(startX), 0), (int(endX), 50),
                       color.astype("uint8").tolist(), -1)
         startX = endX
-    
+
+    # Definir valores rgb mayoritario.
     rgb = centroids[posicionMayor]
-    print ("RED : " + str(rgb[0]))
-    print ("GREEN : " + str(rgb[1]))
-    print ("BLUE : " + str(rgb[2]))
+    global valoresRgbMayoritarios
+    valoresRgbMayoritarios = rgb
+
+    # Descomentar para depurar.
+    """
+    print("RED : " + str(rgb[0]))
+    print("GREEN : " + str(rgb[1]))
+    print("BLUE : " + str(rgb[2]))
+    """
+
     # return the bar chart
     return bar
 
-img = cv2.imread("/Users/Omar/windows7.jpg")
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-img = img.reshape((img.shape[0] * img.shape[1],3)) #represent as row*column,channel number
-clt = KMeans(n_clusters=5) #cluster number
-clt.fit(img)
+# main
+def main():
+    if (rutaImagen != ""):
+        img = cv2.imread(rutaImagen)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-hist = find_histogram(clt)
-bar = plot_colors2(hist, clt.cluster_centers_)
+        img = img.reshape((img.shape[0] * img.shape[1], 3))  # represent as row*column,channel number
+        clt = KMeans(n_clusters=5)  # cluster number
+        clt.fit(img)
 
-plt.axis("off")
-plt.imshow(bar)
-plt.show()
+        hist = find_histogram(clt)
+        bar = plot_colors2(hist, clt.cluster_centers_)  # noqa
+
+        # Descomentar para depurar.
+        """
+        plt.axis("off")
+        plt.imshow(bar)
+        plt.show()
+        """
+
+    # Retornar valores mayoritarios de rgb.
+    return valoresRgbMayoritarios
+
+
+# Llamar a main.
+valoresRgb = main()
+print(json.dumps({"valoresRgb": valoresRgbMayoritarios.tolist()}))
